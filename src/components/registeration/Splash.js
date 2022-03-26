3404f543d53f27b4650f3598ea57b4ed3b80c833
Taher <ttaherhagui@gmail.com>
2022-03-26 13:25:48 +0100

    style splash page for mobile devise version.

--- src/components/registeration/Splash.js
+++ src/components/registeration/Splash.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import LOGO from '../../assets/images/logo.png';
+import './splash.css';
 
 const Splash = () => {
   const { state } = useLocation();
@@ -9,18 +11,48 @@ const Splash = () => {
     alert = state.alert;
   }
   const session = useSelector((state) => state.session);
-  console.log(alert);
+  const adjustSize = () => {
+    const h = window.innerHeight;
+    const splash = document.querySelector('.splash-wrapper');
+    splash.style.height = `${h}px`;
+    const logo = document.querySelector('.logo-wrapper');
+    logo.style.top = `${0.3 * h}px`;
+    const registration = document.querySelector('.registration-links');
+    registration.style.top = `${0.6 * h}px`;
+  };
+
+  useEffect(() => adjustSize(), []);
   return (
     <>
       {!session
     && (
-    <div className="registration-links">
-      <li>
-        <NavLink to="/Signin">Sign in</NavLink>
-      </li>
-      <li>
-        <NavLink to="/Signup">Sing up</NavLink>
-      </li>
+    <div className="splash-wrapper">
+      <div className="app-presentaion">
+        <div className="logo-wrapper">
+          <img src={LOGO} alt="logo" className="logo-img" />
+          <p className="logo-text">CARRENTAL</p>
+        </div>
+        <div className="app-inf">
+          <h1 className="app-name"> CAR RENTAL</h1>
+          <hr />
+          <p className="app-description">
+            Welcome! Now you are thinking about a mean of transportation.
+            Car rental is perhaps the best solution compared to all the other possibilities.
+            High standards of mobility and safety are guaranteed.
+            It makes you free from usage of public transport,
+            and gives you power to ignore the weather conditions.
+            Moreover, it is cheaper than taking a taxi.
+            A rental car makes you free to explore all the country, its cities and towns.
+            No matter whether you are travelling together as a family or company,
+            for a holiday or business,you are welcome to choose any car from our collection.
+            We offer weekly and monthly rates. Feel free to sign up to get the detailed information.
+          </p>
+        </div>
+      </div>
+      <div className="registration-links">
+        <NavLink className="sign up" to="/Signup">SIGN UP</NavLink>
+        <NavLink className="sign in" to="/Signin">LOG IN</NavLink>
+      </div>
     </div>
     )}
       {session
