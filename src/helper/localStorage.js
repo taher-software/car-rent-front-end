537651b96b1472658c457179b319e94833e1c481
Taher <ttaherhagui@gmail.com>
2022-03-28 21:14:04 +0100

    Add helper functions to handle interact with local storage.

---
+++ src/helper/localStorage.js
@@ -0,0 +1,5 @@
+export const populateStorage = (user) => {
+    localStorage.setItem('current_user',JSON.stringify(user));
+};
+
+export const fetchStorage = (key) => (JSON.parse(localStorage.getItem(key)));
