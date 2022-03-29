0715d7d5a6ad30e87b6b4d86f28908d618dac609
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-29 12:39:45 +0300

    Display cars in Desktop version

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
 
 const MyReservations = () => (
-  <h1>MyReservations page</h1>
+  <div>
+    <h1>MyReservations page</h1>
+    <div className="nav-element">
+      <Nav bg="light" className="main-nav flex-column">
+        <Nav.Link href="/">All Cars</Nav.Link>
+        <Nav.Link href="/Reserve">Reserve</Nav.Link>
+        <Nav.Link href="/Myreservations">My Reservations</Nav.Link>
+        <Nav.Link href="/NewCar">Add a Car</Nav.Link>
+        <Nav.Link>Delete a Car</Nav.Link>
+      </Nav>
+    </div>
+  </div>
 );
 
 export default MyReservations;
--- src/components/registeration/Splash.js
+++ src/components/registeration/Splash.js
@@ -51,21 +51,20 @@ const Splash = () => {
          <h3>Please select a car to rent</h3>
          <Row className="scroll-btns">
            <Col>
-             <button className="leftscrollbtn" type="button" onClick={() => scroll(-50)}>&lt;</button>
-             <button className="rightscrollbtn" type="button" onClick={() => scroll(50)}>&gt;</button>
+             <button className="leftscrollbtn" type="button" onClick={() => scroll(-80)}>&lt;</button>
+             <button className="rightscrollbtn" type="button" onClick={() => scroll(80)}>&gt;</button>
            </Col>
          </Row>
          <ul className="cars-list" ref={ref}>
            {cars.map((car) => (
              <li className="car-item" key={car.id}>
-               <a href="/"><img className="car-image" src={car.photo_url} alt="car" width={150} height={150} /></a>
+               <a href="/"><img className="car-image" src={car.photo_url} alt="car" width={180} height={180} /></a>
                <div className="brand-model">
                  <p className="car-brand">{car.brand}</p>
                  -
                  <p className="car-model">{car.model}</p>
                </div>
                <p>{car.model_year}</p>
-               <p className="car-description container-fluid">{car.description}</p>
              </li>
            ))}
 
