003668f8b215f7e0b7f693e5f6af6112bde00a07
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-29 20:33:19 +0300

    fetch elements in mobile

--- src/components/registeration/Splash.js
+++ src/components/registeration/Splash.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
 import { useSelector, useDispatch } from 'react-redux';
 import { Col, Nav, Row } from 'react-bootstrap';
 import fetchAllCars from '../../Redux/cars/fetch/fetchcars';
@@ -19,6 +20,17 @@ const Splash = () => {
   const ref = useRef(null);
   const scroll = (scrollOffset) => {
     ref.current.scrollLeft += scrollOffset;
+  };
+  const menutoggle = () => {
+    const x = document.querySelector('.main-nav');
+    const y = document.querySelector('.hamburger-btn');
+    if (x.style.display === 'flex') {
+      x.style.display = 'none';
+      y.style.color = 'black';
+    } else {
+      x.style.display = 'flex';
+      y.style.color = 'white';
+    }
   };
   return (
     <>
@@ -46,6 +58,7 @@ const Splash = () => {
            <Nav.Link>Delete a Car</Nav.Link>
          </Nav>
        </div>
+       <IconButton type="button" onClick={menutoggle} onKeyDown={menutoggle} style={{ float: 'right' }}><i className="hamburger-btn bi bi-list" style={{ float: 'right', fontSize: '1.5rem' }} /></IconButton>
        <div className="cars-element">
          <h2>Our List Of Cars</h2>
          <h3>Please select a car to rent</h3>
