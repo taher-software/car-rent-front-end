0baa10dcb5ee3d47c55beca3d5105d9ea5de3819
Taher <ttaherhagui@gmail.com>
2022-03-30 15:56:36 +0100

    add header for profile updating and navigation in mobile version.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {
   BrowserRouter as Router, Route, Routes,
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import thunkUser from './Redux/Username/thunk/thunk';
 import Splash from './components/registeration/Splash';
@@ -12,14 +12,77 @@ import MyReservations from './components/Reservations/MyReservations';
 import Reserve from './components/Reservations/reserve';
 import NewCar from './components/cars/newcar';
 import fetchAllCars from './Redux/cars/fetch/fetchcars';
+import DOWN from './assets/images/down.png';
+import LIST from './assets/images/liste.png';
 
 function App() {
   const dispatch = useDispatch();
-
+  const session = useSelector((state) => state.session);
+  const currentUser = useSelector((state) => state.current_user);
+  let userPhoto = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyYOyr5Ec8JLXH9PnLVHZ2QKcW43XQs47vnQ&usqp=CAU';
+  if (currentUser.photo) {
+    userPhoto = currentUser.photo;
+  }
+  const adjustSize = () => {
+    const header = document.querySelector('.header');
+    const menu = document.querySelector('.menu-icon');
+    const profile = document.querySelector('.profile-icon');
+    const w = window.innerWidth;
+    if (w >= 1024) {
+      header.style.padding = '1.25% 0';
+      menu.style.width = '3%';
+      profile.style.width = '4%';
+    }
+  };
   useEffect(() => dispatch(thunkUser()), []);
   useEffect(() => dispatch(fetchAllCars()), []);
+  useEffect(() => adjustSize());
   return (
     <div>
+      <header
+        className="header"
+        style={{
+          display: session ? 'flex' : 'none',
+          backgroundColor: 'rgb(152, 191, 25)',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '5% 0',
+        }}
+      >
+        <img
+          src={LIST}
+          alt="menu-icon"
+          className="menu-icon"
+          width="10%"
+          style={{
+            border: '1px solid rgb(152, 191, 25)',
+            height: '40px',
+            marginLeft: '2.5%',
+          }}
+        />
+        <div
+          className="profile-icon"
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginRight: '2.5%',
+            width: '10%',
+          }}
+        >
+          <img
+            src={userPhoto}
+            className="user-photo"
+            alt="profile"
+            width="75%"
+            style={{
+              border: '1px solid rgb(152, 191, 25)',
+              borderRadius: '50%',
+              height: '32px',
+            }}
+          />
+          <img src={DOWN} className="down-option" alt="account-option" width="25%" height="8px" />
+        </div>
+      </header>
       <Router>
         <Routes>
           <Route path="/" element={<Splash />} />
