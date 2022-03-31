1c905f048427b5f2dc720f54adef6fa6db998f88
Taher <ttaherhagui@gmail.com>
2022-03-31 14:42:02 +0100

    add style function in detail componenent to adjust desktop nav when it is rendered.

--- src/components/cars/detail.js
+++ src/components/cars/detail.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavigationPanel from './navLink';
 
-const Detail = () => (
-  <NavigationPanel />
-);
+const Detail = () => {
+  const adjustSize = () => {
+    const nav = document.querySelector('.nav-wrapper');
+    const h = window.innerHeight;
+    nav.style.height = `${h}px`;
+  };
+  useEffect(() => adjustSize(), []);
+  return (
+    <NavigationPanel />
+  );
+};
 
 export default Detail;
