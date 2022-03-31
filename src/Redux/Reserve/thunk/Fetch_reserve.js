e504a213abe32cf4f4fee8e4be169dfa83b0e794
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-31 19:35:27 +0100

    Setup Redux to Fetch reserve data

---
+++ src/Redux/Reserve/thunk/Fetch_reserve.js
@@ -0,0 +1,14 @@
+import { allReserve, failReserve } from '../actions/action';
+
+const reserveUrl = 'http://[::1]:3001/api/reservations';
+
+const getReserveUrl = () => fetch(reserveUrl);
+
+const fetchReserve = () => (dispatch) => {
+  getReserveUrl()
+    .then((result) => result.json())
+    .then((data) => dispatch(allReserve(data)))
+    .catch((error) => dispatch(failReserve(error)));
+};
+
+export default fetchReserve;
--- src/components/Reservations/NewReserve.js
+++ src/components/Reservations/NewReserve.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { DropdownDate } from 'react-dropdown-date';
+import fetchReserve from '../../Redux/Reserve/thunk/Fetch_reserve';
 import './NewReserve.css';
 
 const DateFormatter = (d, numOfyear) => { // formats a JS date to 'yyyy-mm-dd'
@@ -52,18 +54,22 @@ const cities = [
 ];
 
 const NewReservation = () => {
+  const dispatch = useDispatch();
   const [selectedCity, setSelectedCity] = useState('---Select City---');
   const [selectDate, setSelectedDate] = useState(TodayDate());
   const [Alertmessage, setAlertMessage] = useState('');
+  const reserves = useSelector((state) => state.my_reserves);
+  const currentCar = useSelector((state) => state.current_car);
+  const currentUser = useSelector((state) => state.current_user);
 
   const handleSubmit = async () => {
     if (selectedCity !== '---Select City---') {
       const reservation = {
-        user_id: 1, car_id: 1, start_date: selectDate, city: selectedCity,
+        user_id: currentUser.id, car_id: 1, start_date: selectDate, city: selectedCity,
       };
       const reserveUrl = 'http://localhost:3001/api/reservations';
       const result = await fetch(reserveUrl, {
-        method: 'POST', // or 'PUT'
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -75,48 +81,53 @@ const NewReservation = () => {
       setAlertMessage('Kindly select a city');
     }
   };
-  useEffect(() => {})
+  useEffect(() => dispatch(fetchReserve()), []);
+  console.log(reserves);
+  console.log(currentCar);
+  console.log(currentUser);
   return (
     <>
-      <h1>MyReservations page</h1>
-      <p>{Alertmessage}</p>
-      <form>
-        <div className="reserve-form">
-          <div className="city-style">
-            <select
-              id="cityId"
-              placeholder="city"
-              value={selectedCity}
-              onChange={(e) => setSelectedCity(e.target.value)}
-            >
-              <option>---Select City---</option>
-              {cities.map((city) => <option key={city.id}>{city.name}</option>)}
-            </select>
-          </div>
-          <div className="date-style">
-            <DropdownDate
-              startDate={
+      <div className="new-reserve-container">
+        <h1>MyReservations page</h1>
+        <p>{Alertmessage}</p>
+        <form>
+          <div className="reserve-form">
+            <div className="city-style">
+              <select
+                id="cityId"
+                placeholder="city"
+                value={selectedCity}
+                onChange={(e) => setSelectedCity(e.target.value)}
+              >
+                <option>---Select City---</option>
+                {cities.map((city) => <option key={city.id}>{city.name}</option>)}
+              </select>
+            </div>
+            <div className="date-style">
+              <DropdownDate
+                startDate={
             // optional, if not provided 1900-01-01 is startDate
             TodayDate() // 'yyyy-mm-dd' format only
           }
-              endDate={
+                endDate={
             // optional, if not provided current date is endDate
             extendDate(5) // 'yyyy-mm-dd' format only
           }
-              selectedDate={
+                selectedDate={
             // optional
             selectDate // 'yyyy-mm-dd' format only
           }
-              onDateChange={(date) => {
-                // optional
-                console.log(date);
-                setSelectedDate(formatDate(date));
-              }}
-            />
+                onDateChange={(date) => {
+                  // optional
+                  console.log(date);
+                  setSelectedDate(formatDate(date));
+                }}
+              />
+            </div>
+            <button type="button" onClick={handleSubmit}>Reserve</button>
           </div>
-          <button type="button" onClick={handleSubmit}>Reserve</button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 };
