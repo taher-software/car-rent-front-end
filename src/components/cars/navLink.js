e74fd7920c85562c50ef182755745f039672c85b
Taher <ttaherhagui@gmail.com>
2022-03-31 14:32:35 +0100

    add nav link panel for detail page.

--- src/components/cars/navLink.js
+++ src/components/cars/navLink.js
@@ -1,12 +1,20 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import LOGO from '../../assets/images/logo.png';
+import './navLink.css';
 
 const NavigationPanel = () => (
-  <div>
-    <div className="logo-wrapper">
-      <img src={LOGO} alt="logo" className="logo-img" />
-      <p className="logo-text">CARRENTAL</p>
+  <div className="nav-wrapper">
+    <div className="nav-logo-wrapper">
+      <img src={LOGO} alt="logo" className="nav-logo-img" />
+      <span className="nav-logo-text">CARRENTAL</span>
+    </div>
+    <div className="link-wrapper">
+      <NavLink to="/" className="link-btn">All Cars</NavLink>
+      <NavLink to="/Reserve" className="link-btn">Reserve</NavLink>
+      <NavLink to="/Myreservations" className="link-btn">My Reservations</NavLink>
+      <NavLink to="/Newcar" className="link-btn">Add a Car</NavLink>
+      <NavLink to="/" className="link-btn">Delete</NavLink>
     </div>
   </div>
 );
