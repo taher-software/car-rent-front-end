4648f0b38d114d94dd611f6f8eb8b574adc81424
xtrahuman <okpara_tochukwu@yahoo.com>
2022-04-05 06:59:23 +0100

    Removed unused handleclick function from Navlink

--- src/components/cars/navLink.js
+++ src/components/cars/navLink.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectCar } from '../../Redux/SelectedCar/selectedCar';
 import LOGO from '../../assets/images/logo.png';
 import './navLink.css';
 
 const NavigationPanel = (props) => {
-  const cars = useSelector((state) => state.Cars);
-  const allCars = cars.cars;
-  const dispatch = useDispatch();
   const { deleteHandler } = props;
-  const HandleClick = () => dispatch(selectCar(allCars[0]));
   return (
     <div className="nav-wrapper">
       <div className="nav-logo-wrapper">
@@ -20,7 +14,7 @@ const NavigationPanel = (props) => {
       </div>
       <div className="link-wrapper">
         <NavLink to="/" className="link-btn">All Cars</NavLink>
-        <NavLink to="/Reserve" className="link-btn" onClick={HandleClick}>Reserve</NavLink>
+        <NavLink to="/Reserve" className="link-btn">Reserve</NavLink>
         <NavLink to="/Myreservations" className="link-btn">My Reservations</NavLink>
         <NavLink to="/Newcar" className="link-btn">Add a Car</NavLink>
         <button type="submit" onClick={deleteHandler} className="link-btn delete">Delete</button>
