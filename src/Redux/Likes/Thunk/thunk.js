afe2dd9ff84ed23376ab684be9736c2a8fa9cf80
Taher <ttaherhagui@gmail.com>
2022-04-05 10:03:46 +0100

    Add thunk likes.

---
+++ src/Redux/Likes/Thunk/thunk.js
@@ -0,0 +1,13 @@
+import { startGetLIkes, getLIkes, failGetLikes } from "../Action/action";
+
+const url = 'https://warm-inlet-48309.herokuapp.com/api/likes';
+const fetchLikes = () => (fetch(url));
+const thunkLikes = () => (dispatch) => {
+  dispatch(startGetLIkes());
+  fetchLikes()
+    .then((res) => res.json())
+    .then((result) => dispatch(getLIkes(result)))
+    .catch((err) => dispatch(failGetLikes(err.message)));
+};
+
+export default thunkLikes;
