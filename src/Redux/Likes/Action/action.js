47874c70f8255f247c325cdeb26058952cbd6e1a
Taher <ttaherhagui@gmail.com>
2022-04-05 09:54:00 +0100

    Create likes action state.

---
+++ src/Redux/Likes/Action/action.js
@@ -0,0 +1,16 @@
+export const GET_LIKES_DATA = 'CAR-RENT/LIKES/GET';
+export const START_LIKES_DATA = 'CAR-RENT/LIKES/START';
+export const FAILURE_LIKES_DATA = 'CAR-RENT/LIKES/FAILURE';
+
+export const getLIkes = (payload) => ({
+  type: GET_LIKES_DATA,
+  payload,
+});
+
+export const failGetLikes = (payload) => ({
+  type: FAILURE_LIKES_DATA,
+  payload,
+});
+export const startGetLIkes = () => ({
+  type : START_LIKES_DATA,
+});
