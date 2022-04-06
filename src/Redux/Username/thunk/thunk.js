6171b915589385c936b4f7deb81c9c47706397b4
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-04-06 10:57:14 +0100

    Fix another conflict

--- src/Redux/Username/thunk/thunk.js
+++ src/Redux/Username/thunk/thunk.js
@@ -3,9 +3,9 @@ import { loadUsername, failUsername } from '../actions/action';
 const fetchUser = () => fetch('https://warm-inlet-48309.herokuapp.com/api/users');
 
 const thunkUser = () => (dispatch) => {
-    fetchUser()
-        .then((result) => result.json())
-        .then((res) => dispatch(loadUsername(res)))
-        .catch((error) => dispatch(failUsername(error.message)));
+  fetchUser()
+    .then((result) => result.json())
+    .then((res) => dispatch(loadUsername(res)))
+    .catch((error) => dispatch(failUsername(error.message)));
 };
 export default thunkUser;
--- src/Redux/cars/fetch/fetchcars.js
+++ src/Redux/cars/fetch/fetchcars.js
@@ -3,10 +3,10 @@ import { loadCars, failCars } from '../actions/actions';
 const fetchCars = () => fetch('https://warm-inlet-48309.herokuapp.com/api/cars');
 
 const fetchAllCars = () => (dispatch) => {
-    fetchCars()
-        .then((result) => result.json())
-        .then((res) => dispatch(loadCars(res)))
-        .catch((error) => dispatch(failCars(error.message)));
+  fetchCars()
+    .then((result) => result.json())
+    .then((res) => dispatch(loadCars(res)))
+    .catch((error) => dispatch(failCars(error.message)));
 };
 export default fetchAllCars;
 
